6c2abedae10c5aa7914d9010e928f0d8b9787a4d
Stewart Noll <stewartnoll@gmail.com>
2023-01-20 09:51:28 -0600

    initialize

---
+++ src/summarize.js
@@ -0,0 +1,28 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+function generatePrompt(input) {
+  return `Generat a commit message for the following changes.
+          ${input}`;
+}
+
+export default async function summarize(input) {
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: generatePrompt(input),
+      temperature: 0.6,
+    });
+    console.log(completion.data.choices[0].text);
+  } catch (error) {
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+    }
+  }
+}
